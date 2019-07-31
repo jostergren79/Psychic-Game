deeb91cbb6ca02fe78fdfa1769f9cf199c22a18c
Jason Ostergren <jason@Jasons-MacBook-Air.local>
2019-07-31 17:33:18 -0500

    updated game.js with initial layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// variables
+
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+// function for player to input a letter
+
+function playerInput
+
+// function for computer to randomly press a letter
+
+function computerInput
+
+// function for if the letters match adding to wins counter
+
+function yesMatch
+
+// function for if letters don't match adding to losses counter
+
+function noMatch
+
+// function for guesses left 
+
+function guessesRemaining
+
+// function the returns guesses so far
+
+function guessesSofar
