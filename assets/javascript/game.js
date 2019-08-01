ce8b3973660cc1c522ebafe1038332ea411c3635
Jason Ostergren <jason@Jasons-MacBook-Air.local>
2019-07-31 19:50:43 -0500

    added const's to game.js and started coding the functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,41 @@
-// variables
+// variables defined
 
-const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const playerGuess = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const computerGuess = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const win =0;
+const loss =0;
+const playerWin = document.getElementById(player-Win)
+const playerLose = document.getElementById(player-Lose)
+const guessesLeft = document.getElementById(guesses-Left)
+const guessed = document.getElementById(guess-ed)
 
-// function for player to input a letter
+///--------------------------------///
 
-function playerInput
+// from RPS this function Randomly chooses a choice from the options array. This is the Computer's guess.
+// from RPS This function is run whenever the user presses a key.
 
-// function for computer to randomly press a letter
+document.onkeyup = function(event) {
+const playerGuess = event.key;
+}
 
-function computerInput
+const computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-// function for if the letters match adding to wins counter
+// beginning working on functions.
 
-function yesMatch
+ if ((playerGuess === computerGuess)) {
+        wins++;
+    } else {
+        losses++;
+    }
 
-// function for if letters don't match adding to losses counter
 
-function noMatch
 
-// function for guesses left 
 
-function guessesRemaining
 
-// function the returns guesses so far
 
-function guessesSofar
+
+
+
+
+
+ 
