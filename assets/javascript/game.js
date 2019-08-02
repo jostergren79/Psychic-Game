5f621585aebccde28435d198bd88636924cd14e3
Jason Ostergren <jason@Jasons-MacBook-Air.local>
2019-08-01 19:17:01 -0500

    confirmed win losses are now tabulating correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//--------------------------------//initial variable layout//--------------------------------//
+//--------------------------------//final variable layout//--------------------------------//
 
 const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
@@ -13,27 +13,37 @@ const lettersText = document.getElementById("guessedAlready")
 
 //--------------------------------//initial layout of onkeyupfunction layout//--------------------------------//
 
+//--------------------------------confirmed onkeyup is functioning and win losses are tabulating--------------------------------//
+
+
 document.onkeyup = function(event) {
-    
+
 const playerGuess = event.key;
-let computerGuess = letters[Math.floor(Math.random() * letters.length)]; 
-let tries = (tries - 1);
-    
+
+const computerGuess = letters[Math.floor(Math.random() * letters.length)]; 
+
 if (playerGuess === computerGuess) {
 
-    winsText.textContent = "Wins:" (win + 1);
+    winsText.textContent = "Wins: " + win++;
+    tries--
 } 
 
 if (playerGuess !== computerGuess) {
 
-    lossesText.textContent = "Losses: " + (loss - 1);
+    lossesText.textContent = "Losses: " + loss++;
 }
 }
 
 //--------------------------------//initial update element textContent function layout//--------------------------------//
 
+
+// function updateScore() {
+//     document.querySelector("remainingGuesses").innerHTML = "Guesses Left: " + tries - 1;
+// }
+
 triesText.textContent = "Guesses Left: " + tries;
-lettersText.textContent = "Your Guesses So Far: " + playerGuess;
+
+// lettersText.textContent = "Your Guesses So Far: " + playerGuess;
 
 
 
