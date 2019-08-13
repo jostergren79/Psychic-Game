914dbba12303576b863a39c27199e161c0f66b5d
Jason Ostergren <jason@stp-er-10-129-85-204.wireless.umn.edu>
2019-08-13 09:13:30 -0500

    updated endgame javascript content

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,11 @@
 
 const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-let win = 1;
-let loss = 1;
+let win = 0;
+let loss = 0;
 let tries = 9;
 let playerGuess = "";
-let ListplayerGuess = "";
+let ListplayerGuess = [];
 
 const winsText = document.getElementById("playerWins")
 const lossesText = document.getElementById("playerLoses")
@@ -16,53 +16,52 @@ const lettersText = document.getElementById("guessedAlready")
 triesText.textContent = "Guesses Left: " + tries;
 lettersText.textContent = "Your Guesses So Far: " + playerGuess;
 
+function gameStart(){
+    tries = 9;
+    lettersText.textContent = [];
+    ListplayerGuess = []; 
+    lettersText.textContent = "Your Guesses So Far: " + ListplayerGuess;
+
+ }
 
 // fuction for  onkey event where tries is decrement by one and playerGuess becomes the event key
-
-document.onkeyup = function(event) {
+function updateCounter(){
 
     tries--;
-    
+    triesText.textContent = "Guesses Left: " + tries;
+
+}
+document.onkeyup = function(event) {
+    updateCounter();
+    console.log(tries);
     playerGuess = event.key;
+    ListplayerGuess = ListplayerGuess + " " + playerGuess; //added content ListplayerGuess to create a list of playerGuessed 
+
 
 
 // this defines the computerGuess as a random letter this is determined by the floor or bottom number of the array times the length in this case 1*26
 
 const computerGuess = letters[Math.floor(Math.random() * letters.length)]; 
-
+console.log(computerGuess)
 // this if statement increments the win value a point if there is a match
 
     if (playerGuess === computerGuess) {
-
-        winsText.textContent = "Wins: " + win++;
-    
+        win++
+        winsText.textContent = "Wins: " + win;
+        gameStart();
     } 
 // this if statement increments the loss value a point if there is a mismatch
 
-    if (playerGuess !== computerGuess) {
-
-    lossesText.textContent = "Losses: " + loss++;    
+    if (tries === 0){
+        loss++
+        lossesText.textContent = "Losses: " + loss;  
+        gameStart();
+        alert("GameOver!")
     }
 
 // this information updates text content of elements 
 
-    triesText.textContent = "Guesses Left: " + tries;
-    ListplayerGuess = ListplayerGuess + " " + playerGuess; //added content ListplayerGuess to create a list of playerGuessed 
-    lettersText.textContent = "Your Guesses So Far: " + ListplayerGuess;
+triesText.textContent = "Guesses Left: " + tries;
+lettersText.textContent = "Your Guesses So Far: " + ListplayerGuess;
 
-    if (tries === 0)
-    {
-        alert('you are done!');
-        return;
-    }
-
-}
-
-
-
-
-
-
-
-
- 
+};
